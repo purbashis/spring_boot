229eae155fbae465234deb7f2af100f1d7d17ef5
PURBASHIS BEHERA <purbashis01042000@gmail.com>
2023-03-09 16:59:29 +0530

    Application Properties
    spring.mvc.view.prefix=/pages/
    spring.mvc.view.suffix=.jsp

--- MyWebApp/src/main/java/com/purbashis/demo/HomeController.java
+++ MyWebApp/src/main/java/com/purbashis/demo/HomeController.java
@@ -2,24 +2,22 @@ package com.purbashis.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HomeController
-{
-	@RequestMapping("home")
-	//@ResponseBody
+public class HomeController {
 	
+	@RequestMapping("home")
+//	@ResponseBody
+
 	/*
 	 * @RequestMapping("home") is a Spring Boot annotation used to
 	 *  map HTTP requests to a controller method that handles requests
 	 *   for a specific URL path.
 	 */
-	
-	public String home ()
-	{
-		System.out.println("hi accepted");
-		return"home.jsp";
+	public String home() {
+		System.out.println("it accepted .");
+		return "home";
 	}
 
 }
--- MyWebApp/src/main/webapp/pages/home.jsp
+++ MyWebApp/src/main/webapp/pages/home.jsp
@@ -4,25 +4,21 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<meta charset="UTF-8">
-<title>My webApp</title>
+<title>mywebApp</title>
 </head>
 <body bgcolor=MintCream>
 <center>
     <h1 style="background-color:rgb(0, 255, 127);">I &#128151 java </h1>
-
 </center>
 <hr>
 <center>
 <div style="background-color:powderblue;">
 <button type="button"
 onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
+ Date and Time.</button>
 
 <p id="demo" ></p>
 </div>
 </center>
 </body>
 </html>
-
-<!-- localhost:8080/home -->

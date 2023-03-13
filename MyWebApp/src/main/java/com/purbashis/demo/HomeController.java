5a82ecc6d4ec33afb31785f8bec49593732006f6
PURBASHIS BEHERA <purbashis01042000@gmail.com>
2023-03-13 09:20:19 +0530

    model and view

--- MyWebApp/src/main/java/com/purbashis/demo/HomeController.java
+++ MyWebApp/src/main/java/com/purbashis/demo/HomeController.java
@@ -2,6 +2,8 @@ package com.purbashis.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,14 +21,42 @@ public class HomeController {
 	 *  map HTTP requests to a controller method that handles requests
 	 *   for a specific URL path.
 	 */
-	public String home(HttpServletRequest req,HttpServletResponse res ) //HttpServletRequest request the data 
-	{     HttpSession session =req.getSession();
-		String name = req.getParameter("name");//data coming from the client 
-		System.out.println("it accepted ."+name);
+	//public String home(HttpServletRequest req,HttpServletResponse res ) //HttpServletRequest request the data 
+	//{    // HttpSession session =req.getSession();
+	//	String name = req.getParameter("name");//data coming from the client 
+		//System.out.println("it accepted ."+name);
 		//request dispatcher 
-		session.setAttribute("name", name);
+	//	session.setAttribute("name", name);
 	
-		return "home";
+	//	return "home";
+	
+	
+	
+	
+	
+	
+	//model and view
+	public ModelAndView home(@RequestParam("name")String myName)
+	{
+	ModelAndView mv = new ModelAndView();
+	mv.addObject("name",myName);
+	mv.setViewName("home");
+	return mv;
+	
+	/*
+	 * Spring MVC gives u a special class model and view which can hold 
+	 * two things 
+	 * 1. the view name 
+	 * 2.data (you can add multiple data you want )
+	 */
+	/*
+	 * @RequestParam is a Spring Boot annotation used to extract data from an HTTP request and
+	 *  bind it to a method parameter in a controller or REST endpoint.
+	 *   It is typically used to retrieve query parameters,
+	 *  form data, or HTTP headers from an incoming request.
+	 */
+	
+	
 	}
 
 }
